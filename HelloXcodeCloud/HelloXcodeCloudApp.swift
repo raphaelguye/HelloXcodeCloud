f530c084c2f89fd7385d3c29b3a8586d8c68f6ad
Raphael Guye <raphael.guye@gmail.com>
2021-11-16 08:57:43 +0100

    Initial Commit

---
+++ HelloXcodeCloud/HelloXcodeCloudApp.swift
@@ -0,0 +1,15 @@
+//
+// Created by Raphael Guye on 16.11.21
+// https://github.com/raphaelguye
+// 
+
+import SwiftUI
+
+@main
+struct HelloXcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
